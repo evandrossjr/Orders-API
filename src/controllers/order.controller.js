63b5e2042bf587bc9ab97a81c9edd22e747919ef
Evandro Sacramento II <evandro-dev@outlook.com>
2025-11-30 12:41:23 -0300

    Criar endpoint POST /order
    Implementar mapeamento e transformação do JSON: Ajuste do método createOrder em order.controller para mapear o o pedido pegando os dados do pedido e dos itens e salva-los em suas tabelas respectivamente
    
    Configurar banco de dados (MySQL):  Criação do arquivo db.js para configuração do banco de dados
    
    Salvar pedido transformado no banco de dados
    
    Fixes AB#547
    Fixes AB#548
    Fixes AB#549
    Fixes AB#550

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,30 +1,70 @@
-module.exports = {
+const db = require('../database/db');
 
-    createOrder: (req, res) => {
-        // Criar um pedido
-        res.status(201).send('Pedido criado');
-    },
 
-    getAllOrders: (req, res) => {
-        // Obter todos os pedidos
-        res.status(200).send('Lista de todos os pedidos');
-    },
 
-    getOrderById: (req, res) => {
-        const orderId = req.params.id;  
-        // Obter um pedido pelo número
-        res.status(200).send(`Detalhes do pedido com ID: ${orderId}`);
-    },
+    exports.createOrder = async (req, res)  => {
+        try {
+            const { numeroPedido, valorTotal, dataCriacao, items } = req.body;
 
-    updateOrder: (req, res) => {
-        const orderId = req.params.id;
-        // Atualizar um pedido pelo número
-        res.status(200).send(`Pedido com ID: ${orderId} atualizado`);
-    },
+            if (!numeroPedido || !valorTotal || !dataCriacao || !items || !items) {
+                return res.status(400).json({ message: 'Todos os campos são obrigatórios.' });
+            }
 
-    deleteOrder: (req, res) => {
-        const orderId = req.params.id;
-        // Deletar um pedido pelo número
-        res.status(200).send(`Pedido com ID: ${orderId} deletado`);
-    }
-}
+            const orderMapped = {
+                orderId: numeroPedido,
+                value: valorTotal,
+                creationDates: new Date(dataCriacao).toISOString().slice(0, 19).replace('T', ' '),
+            };
+
+            await db.query (
+                'INSERT INTO orders (orderId, value, creationDate) VALUES (?, ?, ?)',
+                [orderMapped.orderId, orderMapped.value, orderMapped.creationDates]
+            );
+
+            for (const item of items) {
+                const transformedItem = {
+                    productId: Number(item.idItem),
+                    quantity: item.quantidadeItem,
+                    price: item.valorItem
+                };
+
+                await db.query(
+                    'INSERT INTO items (productId, quantity, price) VALUES (?, ?, ?)',
+                    [transformedItem.productId, transformedItem.quantity, transformedItem.price]
+                );
+
+            }
+
+            res.status(201).json({ message: 'Pedido criado com sucesso.', order: orderMapped });
+        } catch (error) {
+            console.error('Erro ao criar o pedido:', error);
+            res.status(500).json({ message: 'Erro ao criar o pedido.' });
+        }
+    };
+
+
+
+
+
+//    //===================
+//    getAllOrders: (req, res) => {
+//        // Obter todos os pedidos
+//        res.status(200).send('Lista de todos os pedidos');
+//        
+//    getOrderById: (req, res) => {
+//        const orderId = req.params.id;  
+//        // Obter um pedido pelo número
+//        res.status(200).send(`Detalhes do pedido com ID: ${orderId}`);
+//    },
+//
+//    updateOrder: (req, res) => {
+//        const orderId = req.params.id;
+//        // Atualizar um pedido pelo número
+//        res.status(200).send(`Pedido com ID: ${orderId} atualizado`);
+//    },
+//
+//    deleteOrder: (req, res) => {
+//        const orderId = req.params.id;
+//        // Deletar um pedido pelo número
+//        res.status(200).send(`Pedido com ID: ${orderId} deletado`);
+//    }
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -5,9 +5,9 @@ const orderController = require('../controllers/order.controller');
 
 
 router.post('/', orderController.createOrder);
-router.get('/list', orderController.getAllOrders);
-router.get('/:id', orderController.getOrderById);
-router.put('/:id', orderController.updateOrder);
-router.delete('/:id', orderController.deleteOrder);
+//router.get('/list', orderController.getAllOrders);
+//router.get('/:id', orderController.getOrderById);
+//router.put('/:id', orderController.updateOrder);
+//router.delete('/:id', orderController.deleteOrder);
 
 module.exports = router;
